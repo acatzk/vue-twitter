453326a5fbe921fd73457fa8cc0726c3273262d5
acatzk <joshuaimalay@gmail.com>
2020-05-01 14:43:15 +0800

    modify the logout button to dropdown with settings select

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -4,13 +4,13 @@
         <v-list-item-avatar>
             <v-img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_oLh-lBWDZkLGJkA9txG8r0DGFr5IkgRBmTpRKfIyH-15m82V&usqp=CAU"></v-img>
         </v-list-item-avatar>
-
+        
         <router-link 
             v-for="(user, index) in users" :key="index"
             :to="`/profile/${user.id}`" 
             style="text-decoration: none;"
         >
-            <v-list-item-content >
+            <v-list-item-content>
                 <v-list-item-title class="subtitle-2" style="position: relative; top: 4px;">
                     {{capitalize(`${user.firstname}`) + " " + capitalize(`${user.lastname}`)}}
                 </v-list-item-title>
@@ -56,15 +56,17 @@
                 <v-list dense>
                     <v-subheader>Options</v-subheader>
                     <v-list-item-group v-model="model" color="blue darken-1">
-                        <v-list-item v-for="(sub, i) in item.subLinks" :key="i">
-                            <v-list-item-icon>
-                                <v-icon>{{ sub.icon }}</v-icon>
-                            </v-list-item-icon>
+                        <div v-for="(sub, i) in item.subLinks" :key="i">
+                            <v-list-item>
+                                <v-list-item-icon>
+                                    <v-icon>{{ sub.icon }}</v-icon>
+                                </v-list-item-icon>
 
-                            <v-list-item-content>
-                                <v-list-item-title>{{ sub.text }}</v-list-item-title>
-                            </v-list-item-content>
-                        </v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>{{ sub.text }}</v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </div>
                     </v-list-item-group>
                 </v-list>
             </v-menu>
@@ -79,10 +81,16 @@
 <script>
 
 import { fb } from '@/firebase'
-import { GET_USER_QUERY } from '@/graphql/queries/getUser'
+import { GET_CURRENT_USER_QUERY } from '@/graphql/queries/getCurrentUser'
+import SkeletonLoader from '@/components/Skeleton-Loader'
 
 export default {
     name: 'Sidebar',
+
+    components: {
+        SkeletonLoader
+    },
+
      data() {
         return {
             user_id: fb.auth().currentUser,
@@ -93,17 +101,12 @@ export default {
                 { title: 'Notifications', icon: 'notifications_none', to: '/notifications' },
                 { title: 'Messages', icon: 'mail_outline', to: '/messages' },
                 { title: 'Bookmarks', icon: 'bookmark_border', to: '/bookmarks' },
-                { title: 'Lists', icon: 'list', to: '/lists' },
-                { title: 'Profile', icon: 'perm_identity', to: '/profile/:id' },
-                { 
-                    title: 'More', 
-                    icon: 'more_horiz',
+                { title: 'Lists', icon: 'list', to: '/lists' },                         
+                { title: 'Profile', icon: 'perm_identity', to: `/profile/:id` },
+                { title: 'More', icon: 'more_horiz',
                     subLinks: [
-                        { text: 'Import', icon: 'perm_identity', to: "/import" },
-                        { text: 'Export', icon: 'perm_identity', to: "/export" },
-                        { text: 'Print', icon: 'perm_identity', to: "/print" },
-                        { text: 'Undo changes', icon: 'perm_identity', to: "/undochanges" },
-                        { text: 'Other contacts', icon: 'perm_identity', to: "others" }
+                        { text: 'Settings', icon: 'perm_identity', to: "/settings" },
+                        { text: 'Logout', icon: 'power_settings_new' }
                     ] 
                 }
             ]
@@ -120,7 +123,7 @@ export default {
 
     apollo: {
         users: {
-            query: GET_USER_QUERY,
+            query: GET_CURRENT_USER_QUERY,
             variables() {
                 return {
                     id: this.user_id ? this.user_id.uid : ''
