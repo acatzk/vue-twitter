986e6c49a014f03238c2dd48fbc12de8e9d1cc31
acatzk <joshuaimalay@gmail.com>
2020-05-09 10:16:19 +0800

    adding component EditProfile for show data thru props

---
+++ src/views/profile/EditProfile.vue
@@ -0,0 +1,120 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="12">
+                    <v-tooltip bottom>
+                        <template v-slot:activator="{ on }">
+                            <v-img
+                                v-on="on"
+                                style="background-color: grey;"
+                                src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg"
+                                height="130"
+                                class="profile-image"
+                            ></v-img>
+                        </template>
+                        <span>Change Cover Photo</span>
+                    </v-tooltip>
+                <v-avatar 
+                    size="110" 
+                    style="position: relative; bottom: 70px; left: 25px; border: 3px solid #fff;">
+                    <v-tooltip bottom>
+                        <template v-slot:activator="{ on }">
+                            <v-img
+                                v-on="on"
+                                :src="userProfile(user)"
+                                class="profile-image"
+                            ></v-img>
+                            <v-btn
+                                class="camera-action" 
+                                icon 
+                                color="white darken-3" 
+                                dark style="position: absolute;"
+                            >
+                                <v-icon>camera_enhance</v-icon>
+                            </v-btn>
+                        </template>
+                        <span>Change Profile</span>
+                    </v-tooltip>
+                </v-avatar>
+            </v-col>
+            <v-col cols="12" sm="6" md="6" class="text-field-area">
+                <v-text-field 
+                    label="Firstname" 
+                    counter=25
+                    v-model="user.firstname"
+                >
+                </v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6" md="6" class="text-field-area">
+                <v-text-field 
+                    label="Lastname" 
+                    v-model="user.lastname"
+                    counter=25
+                >
+                </v-text-field>
+            </v-col>
+            <v-col cols="12" class="text-field-area">
+                <!--   v-model="user.profile.bio" -->
+                <v-textarea
+                    auto-grow
+                    rows="1"
+                    label="Bio"
+                    v-model="user.profile.bio"
+                    counter=160>
+                </v-textarea>
+            </v-col> 
+            <v-col cols="12" class="text-field-area">
+                <v-text-field 
+                    label="Location" 
+                    counter=200
+                    v-model="user.profile.location"
+                ></v-text-field>
+            </v-col>
+            <v-col cols="12" class="text-field-area">
+                <v-text-field 
+                    label="Website" 
+                    counter=100
+                    v-model="user.profile.website"
+                ></v-text-field>
+            </v-col>
+            <v-col cols="12" class="text-field-area">
+                <v-text-field 
+                    label="Birth Date" 
+                    type="date"
+                    v-model="user.profile.birthdate"
+                ></v-text-field>
+            </v-col>
+            <v-col cols="12" class="text-field-area">
+                <v-text-field 
+                    label="Profile URL" 
+                    v-model="user.profile.avatarUrl"
+                    hint="Paste your Profile URL (it's on development)"
+                    persistent-hint
+                ></v-text-field>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'EditProfile',
+
+    props: ['user'],
+
+    methods: {
+         // User Proile Show Detials
+        userProfile(user) {
+            if (user.profile) {
+                if (user.profile.avatarUrl !== '') {
+                    return user.profile.avatarUrl
+                } else {
+                    return 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSycaZi2N67EHasjG_KqowjGtP8WuKNwvlr7GeMUM2fPixnVch_&usqp=CAU'
+                }
+            } else {
+                return 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSycaZi2N67EHasjG_KqowjGtP8WuKNwvlr7GeMUM2fPixnVch_&usqp=CAU'
+            }
+        }
+    }
+}
+</script>
--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -122,101 +122,11 @@
                         </v-card-title>
                         <v-divider></v-divider>
                         <v-card-text style="height: 400px;">
-                            <v-container>
-                                <v-row>
-                                    <v-col cols="12">
-                                          <v-tooltip bottom>
-                                                <template v-slot:activator="{ on }">
-                                                    <v-img
-                                                        v-on="on"
-                                                        style="background-color: grey;"
-                                                        src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg"
-                                                        height="130"
-                                                        class="profile-image"
-                                                    ></v-img>
-                                                </template>
-                                                <span>Change Cover Photo</span>
-                                            </v-tooltip>
-                                        <v-avatar 
-                                            size="110" 
-                                            style="position: relative; bottom: 70px; left: 25px; border: 3px solid #fff;">
-                                            <v-tooltip bottom>
-                                                <template v-slot:activator="{ on }">
-                                                    <v-img
-                                                        v-on="on"
-                                                        :src="userProfile(user)"
-                                                        class="profile-image"
-                                                    ></v-img>
-                                                    <v-btn
-                                                        class="camera-action" 
-                                                        icon 
-                                                        color="white darken-3" 
-                                                        dark style="position: absolute;"
-                                                    >
-                                                        <v-icon>camera_enhance</v-icon>
-                                                    </v-btn>
-                                                </template>
-                                                <span>Change Profile</span>
-                                            </v-tooltip>
-                                        </v-avatar>
-                                    </v-col>
-                                    <v-col cols="12" sm="6" md="6" class="text-field-area">
-                                        <v-text-field 
-                                            label="Firstname" 
-                                            counter=25
-                                            v-model="user.firstname"
-                                        >
-                                        </v-text-field>
-                                    </v-col>
-                                    <v-col cols="12" sm="6" md="6" class="text-field-area">
-                                        <v-text-field 
-                                            label="Lastname" 
-                                            v-model="user.lastname"
-                                            counter=25
-                                        >
-                                        </v-text-field>
-                                    </v-col>
-                                    <v-col cols="12" class="text-field-area">
-                                        <!--   v-model="user.profile.bio" -->
-                                        <v-textarea
-                                            auto-grow
-                                            rows="1"
-                                            label="Bio"
-                                            v-model="user.profile.bio"
-                                            counter=160>
-                                        </v-textarea>
-                                    </v-col> 
-                                    <v-col cols="12" class="text-field-area">
-                                        <v-text-field 
-                                            label="Location" 
-                                            counter=200
-                                            v-model="user.profile.location"
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col cols="12" class="text-field-area">
-                                        <v-text-field 
-                                            label="Website" 
-                                            counter=100
-                                            v-model="user.profile.website"
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col cols="12" class="text-field-area">
-                                        <v-text-field 
-                                            label="Birth Date" 
-                                            type="date"
-                                            v-model="user.profile.birthdate"
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col cols="12" class="text-field-area">
-                                        <v-text-field 
-                                            label="Profile URL" 
-                                            v-model="user.profile.avatarUrl"
-                                            hint="Paste your Profile URL (it's on development)"
-                                            persistent-hint
-                                        ></v-text-field>
-                                    </v-col>
-                                </v-row>
-                             </v-container>
+                            <!-- Show Data -->
+                            <edit-proile 
+                                :user="user"
+                            />
+                            <!-- End Show Data -->
                         </v-card-text>
                     </v-card>
                 </v-dialog>
@@ -330,7 +240,6 @@ import { fb } from '@/firebase'
 import { GET_USER_PROFILE_QUERY } from '@/graphql/queries/getUserProfile'
 import { UPDATE_USER_FULLNAME_MUTATION, UPDATE_USER_PROFILE_MUTATION } from '@/graphql/mutations/updateUserProfile'
 import { FOLLOW_USER_MUTATION, DELETE_FOLLOW_USER_MUTATION, GET_FOLLOW_USER } from '@/graphql/mutations/followUser'
-import UserPosts from './UserPosts'
 
 
 export default {
@@ -338,7 +247,8 @@ export default {
 
     components: {
         Spinner: () => import('@/components/Spinner.vue'),
-        UserPosts
+        UserPosts: () => import('./UserPosts'),
+        EditProile: () => import('./EditProfile')
     },
 
     data() {
@@ -395,7 +305,8 @@ export default {
                 return false
             }
         },
-        // User Proile Show Detials
+
+         // User Proile Show Detials
         userProfile(user) {
             if (user.profile) {
                 if (user.profile.avatarUrl !== '') {
@@ -407,6 +318,7 @@ export default {
                 return 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSycaZi2N67EHasjG_KqowjGtP8WuKNwvlr7GeMUM2fPixnVch_&usqp=CAU'
             }
         },
+
         //  Capital first letter of Text
         capitalize(word) {
             if (typeof word !== 'string') return ''
