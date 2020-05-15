94f88b9a111c26da20f219749d0878f0e146e751
acatzk <joshuaimalay@gmail.com>
2020-05-15 10:52:04 +0800

    adding max-width in sidebar

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,23 +5,9 @@
         <v-row>
           <!-- if the responsive point into medius size close this -->
             <v-col cols="12" md="3"  class="hidden-sm-and-down">
-              <side-bar style="position: fixed;"/> <!--  Sidebar -->
+              <side-bar style="position: fixed; max-width: 220px;"/> <!--  Sidebar -->
            </v-col>
            <!-- End if -->
-
-           <!-- if the responsive point into middium size show this -->
-           <!-- <v-col class="hidden-md-and-up">
-                <v-col md="2">
-                  <side-bar-icon style="max-width: 60px; position: fixed;"/>  
-                </v-col>
-                <v-col md="10" style="margin-left: 70px;">
-                    <div v-if="this.$route.path == '/'" style="margin-right: 50px;">
-                      <tweet-post style="position: relative; bottom: 20px;"/> 
-                    </div>
-                    <router-view style="position: relative; bottom: 20px; margin-right: 50px;"/>
-                </v-col>
-          </v-col> -->
-          <!-- End if -->
 
           <!-- if the responsive point into medius size close this -->
            <v-col cols="12" md="6">
