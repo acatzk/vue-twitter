34d6fd81c35a0f727992cf4800c82f74c687cc0e
acatzk <joshuaimalay@gmail.com>
2020-05-19 09:54:38 +0800

    added subscription folder

---
+++ src/graphql/subscriptions/getAllPosts.js
@@ -0,0 +1,29 @@
+import gql from 'graphql-tag'
+
+export const GET_ALL_POSTS_SUBSCRIPTION = gql`
+subscription getAllPosts {
+  posts(order_by: {created_at: desc}) {
+    id
+    imageUrl
+    caption
+    user_id
+    created_at
+    likes
+    user {
+      id
+      firstname
+      lastname
+      username
+      profile {
+        id
+        avatarUrl
+      }
+    }
+    comments_aggregate {
+      aggregate {
+        count
+      }
+    }
+  }
+}
+`
