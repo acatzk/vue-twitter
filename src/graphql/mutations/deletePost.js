14b1dc25801ddee94a453ec6968b2f77f8ab56c2
acatzk <joshuaimalay@gmail.com>
2020-05-19 09:26:59 +0800

    added delete mutation for comments

--- src/graphql/mutations/deletePost.js
+++ src/graphql/mutations/deletePost.js
@@ -2,10 +2,15 @@ import gql from 'graphql-tag'
 
 export const DELETE_POST_MUTATION = gql`
 mutation ($post_id: uuid!) {
+    delete_comments(where: {post_id: {_eq: $post_id}}) {
+      returning {
+        id
+      }
+    }
     delete_posts(where: {id: {_eq: $post_id}}) {
-        returning {
-            id
-        }
+      returning {
+        id
+      }
     }
 }
 `
