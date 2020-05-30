2a408b8af0fc1b84a1c5113cee47e033ae9d0433
acatzk <joshuaimalay@gmail.com>
2020-05-30 13:13:25 +0800

    create messanges ui

--- src/views/messages/Messages.vue
+++ src/views/messages/Messages.vue
@@ -1,5 +1,29 @@
 <template>
-    <div>
-        <h1>Message</h1>
-    </div>
+  <v-card
+    class="mx-auto"
+    flat
+    outlined
+  >
+    <v-card-title class="row justify-space-between mx-2">
+      <span class="title font-weight-bold">Messages</span>
+      <v-btn icon color="blue darken-1">
+          <v-icon>mail_outline</v-icon>
+      </v-btn>
+    </v-card-title>
+    <v-divider></v-divider>
+    <v-card-text color="blue darken-1">
+        <v-text-field
+            solo-inverted
+            rounded
+            flat
+            dense
+            hide-details
+            label="Search for people and groups"
+            prepend-inner-icon="search"
+            autocomplete="off"
+        ></v-text-field>
+    </v-card-text>
+    <v-divider></v-divider>
+    
+  </v-card>
 </template>
