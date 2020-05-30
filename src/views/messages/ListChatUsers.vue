e9f9f0267ad311eca1060642ea20a46b8e5157d1
acatzk <joshuaimalay@gmail.com>
2020-05-30 13:13:47 +0800

    create list-chat-users component

---
+++ src/views/messages/ListChatUsers.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <v-card-actions>
+            <v-list-item>
+                <v-list-item-avatar>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSycaZi2N67EHasjG_KqowjGtP8WuKNwvlr7GeMUM2fPixnVch_&usqp=CAU">
+                </v-list-item-avatar>
+
+                <v-list-item-content>
+                        <v-list-item-title>
+                            Joshua Galit
+                        </v-list-item-title>
+                        <v-list-item-subtitle>
+                                @angryboy
+                        </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+            <v-divider></v-divider>
+        </v-card-actions>
+    </div>
+</template>
